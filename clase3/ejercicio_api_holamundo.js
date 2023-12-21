980e5602e7857528e0a81c1a085e8c2cdd2f1bae
NicolasPorello <117239658+NicolasPorello@users.noreply.github.com>
2023-12-21 03:46:17 -0300

    agregacion clase 3

---
+++ clase3/ejercicio_api_holamundo.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+const hostname  = "127.0.0.1"//LOCALHOST
+const port = 3000 //PUERTO
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json')
+    res.end(`{"mensaje":"hola mundo"}`);
+})
+server.listen(port,hostname,()=>{
+    console.log("iniciando servidor ")
+})
